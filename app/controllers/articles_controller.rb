e41d004f0eab89c6fad405fef15be385f9df0f61
Omi <bell.tsaeb.shoji+work@gmail.com>
2025-02-09 23:40:43 +0900

    feat: 更新機能を実装しました

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,6 +26,18 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = '更新に成功しました'
+      redirect_to article_path(@article)
+    else
+      flash.now[:alert] = '更新に失敗しました'
+      render :edit, status: :unprocessable_entity
+    end
+
+  end
+
 
   private
   def article_params
