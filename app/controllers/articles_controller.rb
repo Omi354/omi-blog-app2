9ebbf246434018e889a5540ab568786e7d7d6516
Omi <bell.tsaeb.shoji+work@gmail.com>
2025-02-11 01:21:55 +0900

    lint: rubocopを実行しました

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update]
+  before_action :set_article, only: [ :show, :edit, :update ]
 
   def index
     @articles = Article.all
@@ -15,10 +15,10 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = '保存に成功しました'
+      flash[:notice] = "保存に成功しました"
       redirect_to article_path(@article)
     else
-      flash.now[:alert] = '保存に失敗しました'
+      flash.now[:alert] = "保存に失敗しました"
       render :new, status: :unprocessable_entity
     end
   end
@@ -28,19 +28,18 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = '更新に成功しました'
+      flash[:notice] = "更新に成功しました"
       redirect_to article_path(@article)
     else
-      flash.now[:alert] = '更新に失敗しました'
+      flash.now[:alert] = "更新に失敗しました"
       render :edit, status: :unprocessable_entity
     end
-
   end
 
   def destroy
     article = Article.find(params[:id])
     article.destroy!
-    flash[:notice] = '削除しました'
+    flash[:notice] = "削除しました"
     redirect_to root_path, status: :see_other
   end
 
@@ -54,4 +53,3 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 end
-
