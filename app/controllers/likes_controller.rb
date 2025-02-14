2dbacf564b522fca0323429fb2a9c8a7d93c6ddb
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-02-14 21:58:03 +0900

    refactor: LikesControllerで使用していた共通箇所をメソッド化しました

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,19 +1,21 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_article
 
   def create
-    article = Article.find(params[:article_id])
-    article.likes.create(user_id: current_user.id)
+    @article.likes.create(user_id: current_user.id)
     flash[:notice] = "いいねしました"
-    redirect_to article_path(article)
+    redirect_to article_path(@article)
   end
 
   def destroy
-    article = Article.find(params[:article_id])
-    article.likes.find_by(user_id: current_user.id).destroy!
+    @article.likes.find_by(user_id: current_user.id).destroy!
     flash[:notice] = "いいねを削除しました"
-    redirect_to article_path(article), status: :see_other
+    redirect_to article_path(@article), status: :see_other
   end
 
-
+  private
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
 end
