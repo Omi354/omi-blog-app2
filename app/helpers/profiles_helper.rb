71c7cc3217d9d50c817bf8fb7bcee5b1127cd1c0
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-02-16 23:05:14 +0900

    refactor: hamlファイルに記述していた処理の一部をhelperメソッドとして再定義しました

---
+++ app/helpers/profiles_helper.rb
@@ -0,0 +1,9 @@
+module ProfilesHelper
+  def i18n_gender(user)
+    I18n.t("enum.genders.#{user.display_gender}")
+  end
+
+  def map_genders_array
+    Profile.genders.map { |k, v| [I18n.t("enum.genders.#{k}"), k]}
+  end
+end
