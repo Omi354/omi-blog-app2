19906be76753c5750f24fe1e3ccfe8498aaf4358
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-02-24 00:36:23 +0900

    feat: relationshipsテーブルを作成し、フォロー機能とアンフォロー機能を実装しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,11 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :favorite_articles, through: :likes, source: :article
 
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   delegate :display_gender, :display_age, to: :profile, allow_nil: true
 
   def has_written?(article)
@@ -39,6 +44,15 @@ class User < ApplicationRecord
 
   def display_name
     self.profile&.nickname || self.email.split("@").first
+  end
+
+  def follow!(user)
+    self.following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relationship = self.following_relationships.find_by!(following_id: user.id)
+    relationship.destroy!
   end
 
   def prepare_profile
