352168f51157ca5843bbe4e48f4dc33a4cb498b5
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-02-24 12:26:58 +0900

    refactor: タブを部分テンプレート化しました

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -2,7 +2,7 @@ class TimelinesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @articles = Article.where(user_id: current_user.following_ids)
+    @articles = Article.where(user_id: current_user.followings.pluck(:id))
                       .includes(:user)
                       .order(created_at: :desc)
   end
