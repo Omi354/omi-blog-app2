03b46bd4b0c84557b00fe1464a443c259d51bd2f
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-02-26 03:43:20 +0900

    feat: いいね機能と表示切替をAjaxを使って行うように変更しました

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,22 @@
 class LikesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:create, :destroy]
   before_action :set_article
+
+  def show
+    like_status = current_user.has_liked?(@article)
+    render json: { hasLiked: like_status }
+  end
 
   def create
     @article.likes.create(user_id: current_user.id)
     flash[:notice] = "いいねしました"
-    redirect_to article_path(@article)
+    render json: { status: 'ok', message: 'いいねしました' }, status: :created
   end
 
   def destroy
     @article.likes.find_by(user_id: current_user.id).destroy!
     flash[:notice] = "いいねを削除しました"
-    redirect_to article_path(@article), status: :see_other
+    render json: { status: 'ok', message: 'いいねを削除しました' }, status: :ok
   end
 
   private
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,16 +8,62 @@ import "@rails/actiontext"
 import jQuery from "jquery"
 import 'axios'
 
+const csrfToken = document.getElementsByName('csrf-token')[0].content
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken
 window.$ = jQuery
 
+const handleHeartDisplay = (hasLiked) => {
+  if (hasLiked) {
+    $(`.heart-active`).removeClass(`hidden`)
+  } else {
+    $(`.heart-inactive`).removeClass(`hidden`)
+  }
+}
+
+const displayHeartActive = () => {
+  $(`.heart-active`).removeClass(`hidden`)
+  $(`.heart-inactive`).addClass(`hidden`)
+}
+
+const displayHeartInactive = () => {
+  $(`.heart-active`).addClass(`hidden`)
+  $(`.heart-inactive`).removeClass(`hidden`)
+}
+
 document.addEventListener('turbo:load', () => {
-  $('.article_title').on('click', () => {
-    axios.get('/')
+    const dataset = $('#article-show').data()
+    const articleId = dataset.articleId
+
+    axios.get(`/articles/${articleId}/like`)
       .then(response => {
-        console.log(response.data)
+        const hasLiked = response.data.hasLiked
+        handleHeartDisplay(hasLiked)
       })
       .catch(error => {
         console.error('Error:', error)
       })
-  })
+
+    $('.heart-inactive').on('click', () => {
+      axios.post(`/articles/${articleId}/like`)
+        .then(response => {
+          if(response.data.status == 'ok') {
+            displayHeartActive()
+          }
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    })
+
+    $('.heart-active').on('click', () => {
+      axios.delete(`/articles/${articleId}/like`)
+        .then(response => {
+          if(response.data.status == 'ok') {
+            displayHeartInactive()
+          }
+        })
+        .catch(error => {
+          console.error('いいね削除でエラーが発生しました:', error)
+        })
+    })
 })
