76e39b0a4722304635ef765ed4975ded8816b056
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-02-25 10:33:43 +0900

    chore: axiosの設定を行いました

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,9 +5,19 @@ import "controllers"
 import "trix"
 import "@rails/actiontext"
 
-import jquery from "jquery"
-window.$ = jquery
+import jQuery from "jquery"
+import 'axios'
 
-$(function(){
-  alert("jQuery 動いた！")
+window.$ = jQuery
+
+document.addEventListener('turbo:load', () => {
+  $('.article_title').on('click', () => {
+    axios.get('/')
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.error('Error:', error)
+      })
+  })
 })
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
-pin "jquery" # @3.7.1
+pin "jquery"
+pin "axios", to: "https://cdn.jsdelivr.net/npm/axios@1.7.9/dist/axios.min.js"
