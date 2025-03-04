a70100a7f00b65d5f95170a41f2cc45e96436f32
Omi354 <bell.tsaeb.shoji+work@gmail.com>
2025-03-05 01:29:37 +0900

    test: articleコントローラーのテストコードを作成しました

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -80,5 +80,11 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
   
+  # Deviseのテストヘルパーを使用可能にする設定
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :system
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  
   config.include FactoryBot::Syntax::Methods
 end
